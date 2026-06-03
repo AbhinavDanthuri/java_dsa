7709679e1ee7bed8737435dc01066f449a4210fc
AbhinavDanthuri <abhinavdanthuri21@gmail.com>
2026-06-03 16:18:31 +0530

    Initial Java Basics programs

---
+++ java_basics/patterns/RectanglePattern.java
@@ -0,0 +1,12 @@
+public class RectanglePattern {
+    public static void main(String args[]){
+        int n=3;
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
